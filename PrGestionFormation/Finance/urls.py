c4e85cb52874a0abf6a29b8a65251f47ebe8ff2e
iBDEV0101 <ibdev0101@gmail.com>
2025-07-12 22:50:59 +0000

    Mise à jour 22:50 12/07/2025

--- PrGestionFormation/Finance/urls.py
+++ PrGestionFormation/Finance/urls.py
@@ -5,14 +5,14 @@ app_name = 'finance'
 
 urlpatterns = [
     # Vos URLs ici
-    path('scolarite/ajouter/', ScolariteCreateView.as_view(), name='scolarite-create'),
+    path('ajouter/', ScolariteCreateView.as_view(), name='scolarite-create'),
     path('ajax/frais_par_etudiant/', frais_par_etudiant, name='frais_par_etudiant'),
     path("ajax/montant_restant/", montant_restant_pour_frais, name="montant_restant"),
     path("etudiants/search/", search_etudiant_by_matricule, name="etudiant-search-matricule"),
 
-    path('scolarite/<str:type>/ajouter/', ScolariteCreateView.as_view(), name='scolarite-create'),
-    path('scolarite/liste/', ScolariteListView.as_view(), name='scolarite-list'),
-    path('scolarite/liste/<str:type>/', ScolariteListView.as_view(), name='scolarite-list'),
+    path('ajouter/<str:type>', ScolariteCreateView.as_view(), name='scolarite-create'),
+    path('liste/', ScolariteListView.as_view(), name='scolarite-list'),
+    path('liste/<str:type>/', ScolariteListView.as_view(), name='scolarite-list'),
     path('scolarite/<str:type>/<int:pk>/', ScolariteDetailView.as_view(), name='scolarite-detail'),
     path('scolarite/<str:type>/<int:pk>/modifier/', ScolariteUpdateView.as_view(), name='scolarite-update'),
     path('scolarite/<str:type>/<int:pk>/supprimer/', ScolariteDeleteView.as_view(), name='scolarite-delete'),
