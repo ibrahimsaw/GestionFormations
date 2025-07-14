d2db1eadee8269fb417b7525159301b1814917ab
iBDEV0101 <ibdev0101@gmail.com>
2025-07-14 21:12:39 +0000

    Mise à jour 21:12 14/07/2025

--- PrGestionFormation/Finance/urls.py
+++ PrGestionFormation/Finance/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('ajouter/<str:type>', ScolariteCreateView.as_view(), name='scolarite-create'),
     path('liste/', ScolariteListView.as_view(), name='scolarite-list'),
     path('liste/<str:type>/', ScolariteListView.as_view(), name='scolarite-list'),
-    path('scolarite/<str:type>/<int:pk>/', ScolariteDetailView.as_view(), name='scolarite-detail'),
+    path('liste/<str:type>/<int:pk>/', ScolariteDetailView.as_view(), name='scolarite-detail'),
     path('scolarite/<str:type>/<int:pk>/modifier/', ScolariteUpdateView.as_view(), name='scolarite-update'),
     path('scolarite/<str:type>/<int:pk>/supprimer/', ScolariteDeleteView.as_view(), name='scolarite-delete'),
 ]
